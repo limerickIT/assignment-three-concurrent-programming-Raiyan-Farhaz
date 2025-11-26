fccc06bc5495c7cd5271f7a02896932fd4b36585
Raiyan-Farhaz <K00290685@student.tus.ie>
2025-11-26 00:32:28 +0000

    creating a new directory for making images work

--- src/main/java/com/example/assignment_three_zelora/config/WebConfig.java
+++ src/main/java/com/example/assignment_three_zelora/config/WebConfig.java
@@ -1,4 +1,16 @@
 package com.example.assignment_three_zelora.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static.assets.sql/images/");
+    }
 }
